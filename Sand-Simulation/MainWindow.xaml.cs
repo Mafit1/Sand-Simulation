26b5b6a59013beb79a1559d4ab14f0b42e1fc543
Mafit <mafit.tifam@gmail.com>
2025-02-01 12:48:52 +0300

    Initial commit

---
+++ Sand-Simulation/MainWindow.xaml.cs
@@ -0,0 +1,120 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Sand_Simulation
+{
+    public partial class MainWindow : Window
+    {
+        SandGrid _sandGrid;
+        private const int CellSize = 4;
+        private WriteableBitmap _bitmap;
+        private DispatcherTimer _timer;
+
+        private bool _isMouseDown = false;
+
+        private Point _mousePosition;
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            int gridWidth = 200;
+            int gridHeight = 150;
+            _sandGrid = new SandGrid(gridWidth, gridHeight);
+            _bitmap = new WriteableBitmap(
+                gridWidth * CellSize,
+                gridHeight * CellSize,
+                96, 96, PixelFormats.Bgra32, null);
+
+            SandImage.Source = _bitmap;
+
+            _timer = new DispatcherTimer();
+            _timer.Interval = TimeSpan.FromMilliseconds(8);
+            _timer.Tick += (s, e) => Update();
+            _timer.Start();
+        }
+
+
+        private void Update()
+        {
+            _sandGrid.Update();
+            DrawGrid();
+
+            if (_isMouseDown)
+            {
+                var x = (int)_mousePosition.X / CellSize;
+                var y = (int)_mousePosition.Y / CellSize;
+                if (_sandGrid.Grid[x, y] == 0)
+                    _sandGrid.AddSand(x, y);
+            }
+        }
+
+
+        private void DrawGrid()
+        {
+            _bitmap.Lock();
+            try
+            {
+                IntPtr buffer = _bitmap.BackBuffer;
+                int stride = _bitmap.BackBufferStride;
+
+                for (int x = 0; x < _sandGrid.Width; x++)
+                {
+                    for (int y = 0; y < _sandGrid.Height; y++)
+                    {
+                        byte pixel = _sandGrid.Grid[x, y];
+                        uint color = pixel == 1 ? 0xFFFFD700 : 0xFF000000;
+
+                        for (int i = 0; i < CellSize; i++)
+                        {
+                            for (int j = 0; j < CellSize; j++)
+                            {
+                                int posX = x * CellSize + i;
+                                int posY = y * CellSize + j;
+                                unsafe
+                                {
+                                    uint* pixelPtr = (uint*)(buffer + posY * stride + posX * 4);
+                                    *pixelPtr = color;
+                                }
+                            }
+                        }
+                    }
+                }
+                _bitmap.AddDirtyRect(new Int32Rect(0, 0, _bitmap.PixelWidth, _bitmap.PixelHeight));
+            }
+            finally
+            {
+                _bitmap.Unlock();
+            }
+        }
+
+
+        private void SandImage_MouseMove(object sender, MouseEventArgs e)
+        {
+            _mousePosition = e.GetPosition(SandImage);
+        }
+
+
+        private void SandImage_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            _isMouseDown = true;
+            _mousePosition = e.GetPosition(SandImage);
+        }
+
+
+        private void SandImage_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            _isMouseDown = false;
+        }
+    }
+}
---
+++ Sand-Simulation/SandGrid.cs
@@ -0,0 +1,52 @@
+﻿public class SandGrid
+{
+    public byte[,] Grid { get; private set; }
+    public int Width { get; }
+    public int Height { get; }
+
+    public SandGrid(int width, int height)
+    {
+        Width = width;
+        Height = height;
+        Grid = new byte[width, height];
+    }
+
+
+    public void AddSand(int x, int y)
+    {
+        if (x < 0 || x >= Width || y < 0 || y >= Height)
+            return;
+
+        Grid[x, y] = 1;
+    }
+
+
+    public void Update()
+    {
+        for (int y = Height - 2; y >= 0; y--)
+        {
+            for (int x = 0; x < Width; x++)
+            {
+                if (Grid[x, y] == 1)
+                {
+                    // Если под пикселем есть пустое место
+                    if (Grid[x, y + 1] == 0)
+                    {
+                        Grid[x, y] = 0;
+                        Grid[x, y + 1] = 1;
+                    } // Если места нет, падает налево/направо
+                    else
+                    {
+                        int dir = (new Random().Next(0, 2) == 0) ? -1 : 1;
+                        int newX = x + dir;
+                        if (newX >= 0 && newX < Width && Grid[newX, y + 1] == 0)
+                        {
+                            Grid[x, y] = 0;
+                            Grid[newX, y + 1] = 1;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
